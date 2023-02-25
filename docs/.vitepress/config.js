0fed39b10add76ce9a8e425e03a3b0b8bb6ca033
Leighton Hughes <hello@leightonhughes.co.nz>
2023-02-25 14:23:55 +1100

    Update

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  base: 'https://leightonhughes.github.io/'
+  themeConfig: {
+    // Type is `DefaultTheme.Config`
+  }
+})
